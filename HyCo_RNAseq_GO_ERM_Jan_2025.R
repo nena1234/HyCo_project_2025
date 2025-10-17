25011398f5eaa6b18273f4c3686023a42a46172a
nena1234 <35745242+nena1234@users.noreply.github.com>
2025-10-17 11:54:43 +0200

    Update HyCo_RNAseq_GO_ERM_Jan_2025.R

--- HyCo_RNAseq_GO_ERM_Jan_2025.R
+++ HyCo_RNAseq_GO_ERM_Jan_2025.R
@@ -26,38 +26,10 @@ dds <- DESeq(dds)
 
 results <- results(dds, contrast = c("condition", "HyCo", "Normoxic"))
 
-############### Redo the data from Run Zhou for GO##############################
 
-results <- results[
-  !is.na(results$padj) & 
-    !is.na(results$log2FoldChange) & 
-    !is.na(results$pvalue), 
-]
+################# GO for WebGestAlt.com for each Cell Line############
 
-# Filter significant genes (adjust p-value < 0.05 and positive log2 fold change)
-significant_genes <- results[
-  results$padj < 0.05 & results$log2FoldChange > 0, 
-]
-
-# Sort by log2FoldChange in descending order (most upregulated first)
-significant_genes_sorted <- significant_genes[order(significant_genes$log2FoldChange, decreasing = TRUE), ]
-
-# Extract the top 200 upregulated genes
-top_200_upregulated <- head(significant_genes_sorted, 200)
-
-# Create a data frame with only Gene IDs
-top_200_gene_ids <- data.frame(GeneID = rownames(top_200_upregulated))
-
-# Save the Gene IDs to a text file (WebGestalt input format)
-write.table(top_200_gene_ids, "top_200_upregulated_gene_ids.txt", 
-            quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
-
-
-
-
-################# GO and KEGGs for WebGestAlt.com for each Cell Line############
-
-# SK-LMS-1
+################# SK-LMS-1 ###########################################
 
 dds_SK_LMS_1 <- dds[, dds$cell_type == "SK-LMS-1"]
 
@@ -75,11 +47,9 @@ results_SK_LMS_1 <- results_SK_LMS_1[
 ]
 
 # Filter significant genes (adjust p-value < 0.05 and positive log2 fold change)
-upregulated_genes_SK_LMS_1 <- results_SK_LMS_1[
-  results_SK_LMS_1$log2FoldChange > 0, 
-]
+upregulated_genes_SK_LMS_1 <- results_SK_LMS_1[results_SK_LMS_1$log2FoldChange > 0, ]
 
-significant_upregulated_genes_SK_LMS_1 <- upregulated_genes_SK_LMS_1[upregulated_genes_SK_LMS_1 $padj < 0.1,]
+significant_upregulated_genes_SK_LMS_1 <- upregulated_genes_SK_LMS_1[upregulated_genes_SK_LMS_1 $padj < 0.05,]
 
 # Sort by log2FoldChange in descending order (most upregulated first)
 significant_upregulated_genes_sorted_SK_LMS_1 <- significant_upregulated_genes_SK_LMS_1[order(significant_upregulated_genes_SK_LMS_1$padj, decreasing = FALSE), ]
@@ -98,7 +68,7 @@ write.table(top_200_upregulated_ids_SK_LMS_1, "top_200_upregulated_gene_ids_SK-L
 # Filter significant genes (adjust p-value < 0.05 and positive log2 fold change)
 downregulated_genes_SK_LMS_1 <- results_SK_LMS_1[results_SK_LMS_1$log2FoldChange <0, ]
 
-significant_downregulated_genes_SK_LMS_1 <- downregulated_genes_SK_LMS_1[downregulated_genes_SK_LMS_1 $padj < 0.1,]
+significant_downregulated_genes_SK_LMS_1 <- downregulated_genes_SK_LMS_1[downregulated_genes_SK_LMS_1 $padj < 0.05,]
 
 # Sort by log2FoldChange in descending order (most upregulated first)
 significant_downregulated_genes_sorted_SK_LMS_1 <- significant_downregulated_genes_SK_LMS_1[order(significant_downregulated_genes_SK_LMS_1$padj, decreasing = FALSE), ]
@@ -114,14 +84,48 @@ write.table(top_200_downregulated_ids_SK_LMS_1, "top_200_downregulated_gene_ids_
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
 
-############################# STS117 no N4 #####################################
+################### STS117 no N4 #####################################
 
+# Remove samples
+countData_No_STS117_N4 <- countData[, grepl("STS117", colnames(countData))]
+countData_No_STS117_N4 <- countData_No_STS117_N4[, !(colnames(countData_No_STS117_N4) %in% c("Output.STS117.N4.HyCo_2.3439654_S2_L001_sorted.bam", "Output.STS117.N4.S.Norm_2.3439655_S3_L001_sorted.bam"))]
+
+# Verify removal
+sampleNames_No_STS117_N4 <- colnames(countData_No_STS117_N4)
+conditions_No_STS117_N4 <- ifelse(grepl("HyCo", sampleNames_No_STS117_N4), "HyCo", "Normoxic")
+cellTypes_No_STS117_N4 <- ifelse(grepl("STS117", sampleNames_No_STS117_N4), "STS117", NA)
+
+# Remove NA values to keep only valid samples
+valid_samples <- !is.na(cellTypes_No_STS117_N4)
+sampleNames_No_STS117_N4 <- sampleNames_No_STS117_N4[valid_samples]
+conditions_No_STS117_N4 <- conditions_No_STS117_N4[valid_samples]
+cellTypes_No_STS117_N4 <- cellTypes_No_STS117_N4[valid_samples]
+
+colData_No_STS117_N4 <- DataFrame(
+  conditionNoN4 = factor(conditions_No_STS117_N4, levels = c("Normoxic", "HyCo")),
+  cell_typeNoN4 = factor(cellTypes_No_STS117_N4)
+)
+rownames(colData_No_STS117_N4) <- sampleNames_No_STS117_N4  # Assign row names
+
+dds_STS117_No_N4 <- DESeqDataSetFromMatrix(
+  countData = countData_No_STS117_N4,
+  colData = colData_No_STS117_N4,
+  design = ~ conditionNoN4
+)
+
+
+# Run DESeq
+dds_STS117_No_N4 <- DESeq(dds_STS117_No_N4)
+
+results_STS117_No_N4 <- results(dds_STS117_No_N4  , contrast = c("conditionNoN4", "HyCo", "Normoxic"))
+results_STS117_No_N4 <- results_STS117_No_N4[!is.na(results_STS117_No_N4$log2FoldChange) & !is.na(results_STS117_No_N4$pvalue),]
 results_STS117_No_N4 <- results_STS117_No_N4[!is.na(results_STS117_No_N4$log2FoldChange) & !is.na(results_STS117_No_N4$pvalue) & !is.na(results_STS117_No_N4$padj),]
 
+
 ### Top 200 upregulated genes ###
-# Filter significant genes (adjust p-value < 0.1 and positive log2 fold change)
+# Filter significant genes (adjust p-value < 0.05 and positive log2 fold change)
 upregulated_genes_STS117_No_N4 <- results_STS117_No_N4[ results_STS117_No_N4$log2FoldChange > 0, ]
-significant_upregulated_genes_STS117_No_N4 <- upregulated_genes_STS117_No_N4[ upregulated_genes_STS117_No_N4$padj < 0.1 ,]
+significant_upregulated_genes_STS117_No_N4 <- upregulated_genes_STS117_No_N4[ upregulated_genes_STS117_No_N4$padj < 0.1 ,] #### We have increased the padj in order to have enough genes for GO analysis
 
 
 # Sort by log2FoldChange in descending order (most upregulated first)
@@ -140,7 +144,7 @@ write.table(top_200_upregulated_ids_STS117_No_N4, "top_200_upregulated_gene_ids_
 ### Top 200 downregulated genes ###
 # Filter significant genes (adjust p-value < 0.1 and positive log2 fold change)
 downregulated_genes_STS117_No_N4 <- results_STS117_No_N4[ results_STS117_No_N4$log2FoldChange < 0, ]
-significant_downregulated_genes_STS117_No_N4 <- downregulated_genes_STS117_No_N4[ downregulated_genes_STS117_No_N4$padj < 0.1 ,]
+significant_downregulated_genes_STS117_No_N4 <- downregulated_genes_STS117_No_N4[ downregulated_genes_STS117_No_N4$padj < 0.1 ,] #### We have increased the padj in order to have enough genes for GO analysis
 
 
 # Sort by log2FoldChange in descending order (most upregulated first)
